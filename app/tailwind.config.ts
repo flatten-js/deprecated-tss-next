ce162636c0ba7e999dee1228372820609fc17f1c
flatten-js <thetakahito@gmail.com>
2023-12-20 10:27:32 +0900

    init template

---
+++ app/tailwind.config.ts
@@ -0,0 +1,15 @@
+import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [nextui()],
+}
+export default config
