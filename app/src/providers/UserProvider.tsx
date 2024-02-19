8d88fbadc8541c3202d0b98d045a09ec62703b4f
flatten-js <thetakahito@gmail.com>
2024-02-19 10:47:15 +0900

    Some fixes at the refactoring level

--- app/src/providers/UserProvider.tsx
+++ app/src/providers/UserProvider.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import { createContext, useContext } from "react" 
 import { Session } from "next-auth"
 
-export const UserContext = createContext<Session['user']>({})
-
-export function useUser() {
-  return useContext(UserContext)
-}
+import { UserContext } from "@/src/hooks/useUser"
 
 type UserProviderProps = {
   user: Session['user']
